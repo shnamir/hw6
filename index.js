1aa66feff03431910fabc4f74d2859a7b331601b
amir <shiodiyev.amir123@example.com>
2023-05-23 23:00:12 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+// //№1
+// alert('Добро пожаловать')
+
+// let nom = 'Alex'
+// let money = 10000
+// let account = 7777
+
+// let ask  = prompt('Как вас зовут?').toLowerCase().trim()
+// nom=nom.toLowerCase()
+
+// if(ask === nom){
+//    let acc = +prompt('Номер счета?')
+//    if(acc===account){
+//     alert('Все отлично')
+//     let monney = +prompt('Сколько обналичить?')
+//    if(monney<=money){
+//     alert('У вас на балансе еще = '+( money - monney) )
+//     alert('Хорошего дня')
+//    }else{
+//     alert('Недостаточно средств')
+//    }
+//    }else{
+//     alert('Неправильный счет')
+//    }
+// }else{
+//     alert('Пользователь не найден')
+// }
+
+// //№2
+
+// let nom = 'a'
+// let age = 20
+// let money = 100
+
+// let ask = prompt('Как вас зовут?').slice(0,1).toLowerCase().trim()
+// if (ask === nom){
+//     alert('Все отлично')
+//     let agge = prompt('Сколько вам лет?')
+//     if(agge >= 20 && agge <= 40){
+//         alert('Хорошо')
+//         let monney = prompt('Сколько у вас денег?')
+//         if (money<=monney){
+//             alert('Заходите')
+//         }else{alert('У вас мало денег')}
+//     }else{alert('Ваш возраст не потходит')}
+// }else{alert('Сегодня день буквы а')}
+
